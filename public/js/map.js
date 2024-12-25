a14351580b0148d2d875e5f8d151d8c5abc89777
Harsh093-k <harshkashyap701153@gmail.com>
2024-12-25 13:34:54 +0530

    all done

--- public/js/map.js
+++ public/js/map.js
@@ -4,12 +4,12 @@
 
           const map = new mapboxgl.Map({
               container: 'map', // container ID
-              center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+              center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
               zoom: 9 // starting zoom
           });
  
 const marker = new mapboxgl.Marker({ color:"red"})
-.setLngLat(coordinates) 
+.setLngLat(listing.geometry.coordinates) 
 .setPopup(new mapboxgl.Popup({offset: 25})
-.setHTML(`<p>Exact Location provided after </p>`)) 
+.setHTML(`<h4>${listing.location}</h4><p>Exact Location provided after </p>`)) 
 .addTo(map);        
