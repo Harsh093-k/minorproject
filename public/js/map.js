60f5f56e81df03cf773a625e39ca8586682f7884
Harsh093-k <harshkashyap701153@gmail.com>
2024-12-25 14:14:13 +0530

    all done

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,12 @@
 
        
-        mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = mapToken;
 
-          const map = new mapboxgl.Map({
-              container: 'map', // container ID
-              center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-              zoom: 9 // starting zoom
-          });
- 
+const map = new mapboxgl.Map({
+container: 'map', // container ID
+center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+zoom: 9 // starting zoom
+});
 const marker = new mapboxgl.Marker({ color:"red"})
 .setLngLat(listing.geometry.coordinates) 
 .setPopup(new mapboxgl.Popup({offset: 25})
